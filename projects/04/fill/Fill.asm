1fd4a99fd932df1125ea8c0abcebd3f11bdb7fcd
John Xia <john.danger.xia@gmail.com>
2015-09-15 17:56:48 -0400

    We have a computer??

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,67 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(LOOP)
+@KBD // A = KBD
+D=M // D = *KBD
+@CLEAR  // A = LOOP
+D;JEQ  // if D == 0 (no keypress), jump to CLEAR
+@FILL
+0;JMP  // else jump to FILL
+
+(CLEAR)
+@R3
+M=0 // set color to 0000 0000 0000 0000
+@COLOR
+0;JMP // color the screen
+
+(FILL)
+@R3
+M=0 // set color to 1111 1111 1111 1111
+M=!M
+@COLOR
+0;JMP // color the screen
+
+(COLOR)
+@SCREEN
+D=A  // D = SCREEN
+@R0
+M=D // *R0 = SCREEN
+@256
+D=A // D = 256
+@R1
+M=D  // *R1 = 256
+
+(ROWLOOP)
+@R1
+D=M  // D = *R1
+@LOOP
+D;JEQ // if we have no rows left to color, go to loop
+@32
+D=A // D = 32
+@R2 // init loop variable for chunkloop
+M=D // *R2 = 32; number of 16-bit numbers we need to put in to get a row
+@R1
+M=M-1 // decrement row counter
+
+(CHUNKLOOP)
+@R2
+D=M // D = *R2 = remaining chunks in row
+@ROWLOOP
+D;JEQ  // done with this row! go to next one!
+@R3
+D=M // D = *R3 = color of fill
+@R0  // A = R0 (memory address of pixels we want to poke)
+A=M
+M=D // toggle fill.
+@R0
+M=M+1 // advance screen pointer
+@R2
+M=M-1  // decrement num chunks left
+@CHUNKLOOP
+0;JMP  // keep filling in chunks
+
+@D
+@LOOP
+0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// While R0 >= 0:
+// R0 -= 1
+// R2 += R1
+
+@R2
+M=0
+
+(ADDLOOP)
+@R0  // A = R0
+D=M  // D = *R0
+@END
+D;JEQ // jump to end if D = 0
+@R1  // A = R1
+D=M  // D = *R1
+@R2  // A = R2
+M=D+M  // *R2 = *R2 + *R1
+@R0  // A = R0
+M=M-1 // *R0 -= 1
+@ADDLOOP
+0;JMP
+
+(END)
+@END
+0;JMP
