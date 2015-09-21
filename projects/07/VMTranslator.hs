2663c79dfc2894e51e61ae82076b11a7c1b67102
John Xia <john.danger.xia@gmail.com>
2015-09-21 17:37:27 -0400

    clean up a little bit

--- projects/07/VMTranslator.hs
+++ projects/07/VMTranslator.hs
@@ -7,162 +7,115 @@ import System.Environment
 class Assemblable a where
   toAsm :: a -> String
 
+popSP :: [String]
+popSP = ["@SP", "M=M-1", "A=M"]
+ 
+pushSP :: [String]
+pushSP = ["@SP", "M=M+1"]
+
 instance Assemblable Command where
-  toAsm (Push Constant (Just n)) = unlines
-                                   [
-                                     "@" ++ show n, -- A = n
-                                     "D=A",  -- D = n
-                                     "@SP",
-                                     "A=M",
-                                     "M=D",
-                                     "@SP",
-                                     "M=M+1"
-                                   ]
-  toAsm Add = unlines
-              [
-                "@SP", -- M = &y+1
-                "M=M-1", -- M = &y
-                "A=M", -- A = &y
-                "D=M", -- D = M = y
-                "@SP",
-                "M=M-1", -- M = *x
-                "A=M", -- A = *x
-                "M=D+M", -- x = y + x
-                "@SP",
-                "M=M+1"
-              ]
-  toAsm Sub = unlines
-              [
-                "@SP",
-                "M=M-1", -- M = &y
-                "A=M",
-                "D=M", -- D = M = y
-                "@SP",
-                "M=M-1",
-                "A=M", -- M = x
-                "M=M-D", -- x = x - y
-                "@SP",
-                "M=M+1"
-              ]
-  toAsm Neg = unlines
-              [
-                "@SP", -- A = &(&y)
-                "M=M-1", -- M = &y
-                "A=M", -- A = &y
-                "M=-M", -- y = -y
-                "@SP",
-                "M=M+1"
-              ]
-  toAsm (Eq jId) = unlines
-                   [
-                     "@SP",
-                     "M=M-1", -- M = &y
-                     "A=M",
-                     "D=M", -- D = M = y
-                     "@SP",
-                     "M=M-1",
-                     "A=M", -- M = x
-                     "D=M-D", -- D = x - y
-                     "@eqJmp"++show jId,
-                     "D;JEQ", -- x = x - y
-                     "@SP",
-                     "A=M",
-                     "M=0",
-                     "@endEqJmp"++show jId,
-                     "0;JMP",
-                     "(eqJmp"++show jId++")",
-                     "@SP",
-                     "A=M",
-                     "M=-1",
-                     "(endEqJmp"++show jId++")",
-                     "@SP",
-                     "M=M+1"
-                   ]
-  toAsm (Gt jId) = unlines
-                   [
-                     "@SP",
-                     "M=M-1", -- M = &y
-                     "A=M",
-                     "D=M", -- D = M = y
-                     "@SP",
-                     "M=M-1",
-                     "A=M", -- M = x
-                     "D=M-D", -- D = x - y
-                     "@gtJmp"++show jId,
-                     "D;JGT", -- x = x - y
-                     "@SP",
-                     "A=M",
-                     "M=0",
-                     "@endGtJmp"++show jId,
-                     "0;JMP",
-                     "(gtJmp"++show jId++")",
-                     "@SP",
-                     "A=M",
-                     "M=-1",
-                     "(endGtJmp"++show jId++")",
-                     "@SP",
-                     "M=M+1"
-                   ]
-  toAsm (Lt jId) = unlines
-                   [
-                     "@SP",
-                     "M=M-1", -- M = &y
-                     "A=M",
-                     "D=M", -- D = M = y
-                     "@SP",
-                     "M=M-1",
-                     "A=M", -- M = x
-                     "D=M-D", -- D = x - y
-                     "@ltJmp"++show jId,
-                     "D;JLT", -- x = x - y
-                     "@SP",
-                     "A=M",
-                     "M=0",
-                     "@endLtJmp"++show jId,
-                     "0;JMP",
-                     "(ltJmp"++show jId++")",
-                     "@SP",
-                     "A=M",
-                     "M=-1",
-                     "(endLtJmp"++show jId++")",
-                     "@SP",
-                     "M=M+1"
-                   ]
-  toAsm And = unlines
-              [
-                "@SP",
-                "M=M-1",
-                "A=M",
-                "D=M",
-                "@SP",
-                "M=M-1",
-                "A=M",
-                "M=D&M",
-                "@SP",
-                "M=M+1"
-              ]
-  toAsm Or = unlines
-             [
-               "@SP",
-               "M=M-1",
-               "A=M",
-               "D=M",
-               "@SP",
-               "M=M-1",
-               "A=M",
-               "M=D|M",
-               "@SP",
-               "M=M+1"
-             ]
-  toAsm Not = unlines
-              [
-                "@SP", -- A = &(&y)
-                "M=M-1", -- M = &y
-                "A=M", -- A = &y
-                "M=!M", -- y = -y
-                "@SP",
-                "M=M+1"
-              ]
+  toAsm (Push Constant (Just n)) =
+    unlines $
+    [
+      "@" ++ show n, -- A = n
+      "D=A",  -- D = n
+      "@SP",
+      "A=M",
+      "M=D"
+    ] ++ pushSP
+  toAsm Add =
+    unlines $
+    popSP ++
+    "D=M" :
+    popSP ++
+    "M=D+M" : -- x = y + x
+    pushSP
+  toAsm Sub =
+    unlines $
+    popSP ++
+    "D=M" : -- D = M = y
+    popSP ++
+    "M=M-D" :
+    pushSP -- x = x - y
+  toAsm Neg = unlines $ popSP ++ "M=-M" : pushSP
+  toAsm (Eq jId) =
+    unlines $
+    popSP ++
+    "D=M" : -- D = M = y
+    popSP ++
+    [
+      "D=M-D", -- D = x - y
+      "@eqJmp"++show jId,
+      "D;JEQ", -- x = x - y
+      "@SP",
+      "A=M",
+      "M=0",
+      "@endEqJmp"++show jId,
+      "0;JMP",
+      "(eqJmp"++show jId++")",
+      "@SP",
+      "A=M",
+      "M=-1",
+      "(endEqJmp"++show jId++")"
+    ] ++
+    pushSP
+  toAsm (Gt jId) = 
+    unlines $
+    popSP ++
+    "D=M" : -- D = M = y
+    popSP ++
+    [
+      "D=M-D", -- D = x - y
+      "@gtJmp"++show jId,
+      "D;JGT", -- x = x - y
+      "@SP",
+      "A=M",
+      "M=0",
+      "@endGtJmp"++show jId,
+      "0;JMP",
+      "(gtJmp"++show jId++")",
+      "@SP",
+      "A=M",
+      "M=-1",
+      "(endGtJmp"++show jId++")"
+    ] ++
+    pushSP
+  toAsm (Lt jId) = 
+    unlines $
+    popSP ++
+    "D=M" : -- D = M = y
+    popSP ++
+    [
+      "D=M-D", -- D = x - y
+      "@ltJmp"++show jId,
+      "D;JLT", -- x = x - y
+      "@SP",
+      "A=M",
+      "M=0",
+      "@endLtJmp"++show jId,
+      "0;JMP",
+      "(ltJmp"++show jId++")",
+      "@SP",
+      "A=M",
+      "M=-1",
+      "(endLtJmp"++show jId++")"
+    ] ++
+    pushSP
+  toAsm And =
+    unlines $
+    popSP ++
+    "D=M" :
+    popSP ++
+    "M=D&M" :
+    pushSP
+  toAsm Or =
+    unlines $
+    popSP ++
+    "D=M" :
+    popSP ++
+    "M=D|M" :
+    pushSP
+  toAsm Not = unlines $ popSP ++ "M=!M" : pushSP
 
 main :: IO ()
 main = do
