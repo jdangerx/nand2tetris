59dd85493806c52fea33e8fbd7f20879f6c26bf2
John Xia <john.danger.xia@gmail.com>
2015-09-22 13:15:37 -0400

    Program flow works!

---
+++ projects/08/VMParse.hs
@@ -0,0 +1,115 @@
+module VMParse where
+
+import Data.Char (isSpace)
+import Data.Maybe (catMaybes)
+import Text.Parsec
+
+data Command = Push Segment
+             | Pop Segment
+             | Add
+             | Sub
+             | Neg
+             | Eq Filename JumpId
+             | Gt Filename JumpId
+             | Lt Filename JumpId
+             | And
+             | Or
+             | Not
+             | Label String Filename
+             | Goto String Filename
+             | IfGoto String Filename
+             deriving Show
+
+data Segment = Argument Index
+             | Local Index
+             | Static Filename Index
+             | Constant Index
+             | This Index
+             | That Index
+             | Pointer Index
+             | Temp Index
+             deriving Show
+
+type Index = Maybe Int
+
+type Filename = String
+
+type JumpId = Int
+
+comment :: Parsec String (Filename, Int) String
+comment = do
+  string "//"
+  many $ noneOf "\n\r"
+
+spaces1 :: Parsec String (Filename, Int) ()
+spaces1 = do
+  satisfy isSpace
+  spaces
+
+segment :: Parsec String (Filename, Int) (Index -> Segment)
+segment =
+  (try (string "argument") >> return Argument)
+  <|> (try (string "local") >> return Local)
+  <|> try (pure Static <*> (string "static" >> fst <$> getState))
+  <|> (try (string "constant") >> return Constant)
+  <|> (try (string "this") >> return This)
+  <|> (try (string "that") >> return That)
+  <|> (try (string "pointer") >> return Pointer)
+  <|> (try (string "temp") >> return Temp)
+
+index :: Parsec String (Filename, Int) Index
+index = do
+  num <- optionMaybe (many digit)
+  return $ read <$> num
+
+pushPop :: Parsec String (Filename, Int) Command
+pushPop = do
+  cmd <- (try (string "push") >> return Push) <|> (try (string "pop") >> return Pop)
+  spaces1
+  seg <- segment
+  spaces1
+  ind <- index
+  return $ cmd . seg $ ind
+
+arithmetic :: Parsec String (Filename, Int) Command
+arithmetic =
+  try (string "add" >> return Add)
+  <|> try (string "sub" >> return Sub)
+  <|> try (string "neg" >> return Neg)
+  <|> try (pure (uncurry Eq) <*>
+           (string "eq" >> modifyState ((+ 1) <$>) >> getState))
+  <|> try (pure (uncurry Gt) <*>
+           (string "gt" >> modifyState ((+ 1) <$>) >> getState))
+  <|> try (pure (uncurry Lt) <*>
+           (string "lt" >> modifyState ((+ 1) <$>) >> getState))
+  <|> try (string "and" >> return And)
+  <|> try (string "or" >> return Or)
+  <|> try (string "not" >> return Not)
+
+
+flow :: Parsec String (Filename, Int) Command
+flow =
+  do
+    cmd <- (string "label" >> return Label)
+          <|> (string "goto" >> return Goto)
+          <|> (string "if-goto" >> return IfGoto)
+    spaces1
+    labelName <- many (alphaNum <|> oneOf "_.$:")
+    filename <- fst <$> getState
+    return $ cmd labelName filename
+
+command :: Parsec String (Filename, Int) (Maybe Command)
+command =
+  do
+    cmd <- optionMaybe (try arithmetic
+                       <|> try pushPop
+                       <|> try flow)
+    optional comment
+    many (noneOf "\n\r")
+    return cmd
+
+parseVM :: Parsec String (Filename, Int) [Command]
+parseVM =
+  do
+    cmds <- sepBy command (oneOf "\n\r") <* eof
+    return $ catMaybes cmds
---
+++ projects/08/VMTranslator.hs
@@ -0,0 +1,180 @@
+module Main where
+
+import VMParse
+
+import Control.Monad
+import System.Directory
+import System.Environment
+import System.FilePath
+import Text.Parsec
+
+decSP :: [String]
+decSP = ["@SP", "M=M-1", "A=M"]
+
+incSP :: [String]
+incSP = ["@SP", "M=M+1"]
+
+pushDSP :: [String]
+pushDSP = ["@SP", "A=M", "M=D"] ++ incSP
+
+pushSeg :: Segment -> [String]
+pushSeg (Constant (Just n)) =
+  ["@" ++ show n, "D=A"]
+pushSeg (Local (Just n)) =
+  ["@LCL", "D=M", "@" ++ show n, "A=D+A", "D=M"]
+pushSeg (Argument (Just n)) =
+  ["@ARG", "D=M", "@" ++ show n, "A=D+A", "D=M"]
+pushSeg (This (Just n)) =
+  ["@THIS", "D=M", "@" ++ show n, "A=D+A", "D=M"]
+pushSeg (That (Just n)) =
+  ["@THAT", "D=M", "@" ++ show n, "A=D+A", "D=M"]
+pushSeg (Temp (Just n)) =
+  ["@5", "D=A", "@" ++ show n, "A=D+A", "D=M"]
+pushSeg (Pointer (Just n)) =
+  ["@THIS", "D=A", "@" ++ show n, "A=D+A", "D=M"]
+pushSeg (Static filename (Just n)) =
+  ["@" ++ filename ++ "." ++ show n, "D=M"]
+pushSeg _ = []
+
+popToR13 :: [String]
+popToR13 = ["@SP", "M=M-1", "A=M", "D=M", "@R13", "M=D"] -- R13 = **SP
+
+putR13IntoR14 :: [String]
+putR13IntoR14 = ["@R13", "D=M", "@R14", "A=M", "M=D"]
+
+popSeg :: Segment -> [String]
+popSeg (Local (Just n)) =
+  ["@LCL", "D=M", "@" ++ show n, "D=D+A", "@R14", "M=D"]
+popSeg (Argument (Just n)) =
+  ["@ARG", "D=M", "@" ++ show n, "D=D+A", "@R14", "M=D"]
+popSeg (This (Just n)) =
+  ["@THIS", "D=M", "@" ++ show n, "D=D+A", "@R14", "M=D"]
+popSeg (That (Just n)) =
+  ["@THAT", "D=M", "@" ++ show n, "D=D+A", "@R14", "M=D"]
+popSeg (Temp (Just n)) =
+  ["@5", "D=A", "@" ++ show n, "D=D+A", "@R14", "M=D"]
+popSeg (Pointer (Just n)) =
+  ["@THIS", "D=A", "@" ++ show n, "D=D+A", "@R14", "M=D"]
+popSeg (Static filename (Just n)) =
+  ["@" ++ filename ++ "." ++ show n, "D=A", "@R14", "M=D"]
+popSeg _ = []
+
+class Assemblable a where
+  toAsm :: a -> [String]
+
+instance Assemblable Command where
+  toAsm (Label lname fname) = ["(" ++ fname ++ "." ++ lname ++")"]
+  toAsm (Goto lname fname) = ["@" ++ fname ++ "." ++ lname, "0;JMP"]
+  toAsm (IfGoto lname fname) =
+    decSP ++
+    [
+      "D=M",
+      "@" ++ fname ++ "." ++ lname,
+      "D;JNE"
+    ]
+  toAsm (Push seg) =
+    pushSeg seg ++ pushDSP
+  toAsm (Pop seg) =
+    popToR13 ++ popSeg seg ++ putR13IntoR14
+  toAsm Add =
+    decSP ++ "D=M" : decSP ++ "M=D+M" : incSP
+  toAsm Sub =
+    decSP ++ "D=M" : decSP ++ "M=M-D" : incSP
+  toAsm Neg = decSP ++ "M=-M" : incSP
+  toAsm (Eq fn jId) =
+    decSP ++
+    "D=M" : -- D = M = y
+    decSP ++
+    [
+      "D=M-D", -- D = x - y
+      "@" ++ fn ++ "._eqJmp_"++show jId,
+      "D;JEQ", -- x = x - y
+      "@SP",
+      "A=M",
+      "M=0",
+      "@" ++ fn ++ "._endEqJmp_"++show jId,
+      "0;JMP",
+      "(" ++ fn ++ "._eqJmp_"++show jId++")",
+      "@SP",
+      "A=M",
+      "M=-1",
+      "(" ++ fn ++ "._endEqJmp_"++show jId++")"
+    ] ++
+    incSP
+  toAsm (Gt fn jId) =
+    decSP ++
+    "D=M" : -- D = M = y
+    decSP ++
+    [
+      "D=M-D", -- D = x - y
+      "@" ++ fn ++ "._gtJmp_"++show jId,
+      "D;JGT", -- x = x - y
+      "@SP",
+      "A=M",
+      "M=0",
+      "@" ++ fn ++ "._endGtJmp_"++show jId,
+      "0;JMP",
+      "(" ++ fn ++ "._gtJmp_"++show jId++")",
+      "@SP",
+      "A=M",
+      "M=-1",
+      "(" ++ fn ++ "._endGtJmp_"++show jId++")"
+    ] ++
+    incSP
+  toAsm (Lt fn jId) =
+    decSP ++
+    "D=M" : -- D = M = y
+    decSP ++
+    [
+      "D=M-D", -- D = x - y
+      "@" ++ fn ++ "._ltJmp_"++show jId,
+      "D;JLT", -- x = x - y
+      "@SP",
+      "A=M",
+      "M=0",
+      "@" ++ fn ++ "._endLtJmp_"++show jId,
+      "0;JMP",
+      "(" ++ fn ++ "._ltJmp_"++show jId++")",
+      "@SP",
+      "A=M",
+      "M=-1",
+      "(" ++ fn ++ "._endLtJmp_"++show jId++")"
+    ] ++
+    incSP
+  toAsm And =
+    decSP ++ "D=M" : decSP ++ "M=D&M" : incSP
+  toAsm Or =
+    decSP ++ "D=M" : decSP ++ "M=D|M" : incSP
+  toAsm Not = decSP ++ "M=!M" : incSP
+
+parseFile :: FilePath -> IO (Either ParseError [Command])
+parseFile infile =
+  liftM
+  (runParser parseVM (takeBaseName infile, 0) infile)
+  (readFile infile)
+
+main :: IO ()
+main = do
+  [infile] <- getArgs
+  isFile <- doesFileExist infile
+  isDir <- doesDirectoryExist infile
+  infiles <- if isFile
+            then return [infile]
+            else if isDir
+                 then map (combine infile) . filter ((== ".vm") . takeExtension)
+                      <$> getDirectoryContents infile
+                 else return []
+  outfile <-
+    let
+      asm = (takeBaseName . dropTrailingPathSeparator $ infile) -<.> "asm"
+    in
+     if isFile
+     then return $ combine (takeDirectory infile) asm
+             else if isDir
+                  then return $ combine infile asm
+                  else return ""
+  vmCmds <- (concat <$>) <$> (sequence <$> mapM parseFile infiles)
+  case vmCmds of
+   Left err -> print err
+   Right c -> writeFile outfile (unlines . concatMap toAsm $ c)
+  return ()
